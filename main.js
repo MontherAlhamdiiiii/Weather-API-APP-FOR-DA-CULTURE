346964b6a82daaa6ea00436ff29ef05332f2545b
MontherAlhamdiiiii <kaismohammed266@gmail.com>
2026-09-25 01:03:48 +0200

    First API APP

---
+++ main.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector(".searchbtn");
+let input = document.querySelector("input");
+
+let temp = document.querySelector(".main h2");
+let city = document.querySelector(".main h3");
+
+let both = document.querySelectorAll(".box p:first-of-type");
+
+let img = document.querySelector(".state-img img");
+console.log(img);
+
+btn.onclick = async function () {
+  if (input.value !== "") {
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=11e87b0a073f706d7ee267df03dcbcbb`,
+    );
+    let result = await response.json();
+    temp.innerHTML = `${result.main.temp}°C`;
+    city.innerHTML = `${result.name}`;
+    both[0].innerHTML = `${result.main.humidity} %`;
+    both[1].innerHTML = `${result.wind.speed} km/h`;
+    img.src = `images/${result.weather[0].main.toLowerCase()}.png`;
+  }
+};
